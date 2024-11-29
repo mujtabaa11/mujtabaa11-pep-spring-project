83236e82626e25644110c160edf24df1b3d1ff03
Mohamed Mujtaba <Mohamed.Mugtaba11@gmail.com>
2024-11-29 20:47:25 +0000

    First commit: edited repository interfaces

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,4 +1,13 @@
 package com.example.repository;
 
-public interface AccountRepository {
+import com.example.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+    // Custom query method to find an Account by username
+    Optional<Account> findByUsername(String username);
 }
